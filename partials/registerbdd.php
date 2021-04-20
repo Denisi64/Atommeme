c3319e72c0f83d9170056e2b8a6a863178523e3f
Denisi64 <simon.denis64@gmail.com>
2021-04-20 17:31:25 +0200

    update register
    registration of a user and insert it in the database with date, role and base image

--- partials/registerbdd.php
+++ partials/registerbdd.php
@@ -1,11 +1,17 @@
 <?php
 include '../partials/conectionbdd.php';
+include '../partials/timezone.php';
 if (isset($_POST["pseudo"]) && ($_POST["pseudo"] != "")) {
 
 
     $pseudo = $_POST["pseudo"];
     $password = $_POST["password"];
-    $sql = "INSERT INTO user (username, password) VALUES ('$pseudo', '$password')";
+    $role = 2;
+    $img = 'default.png';
+    $date= date("Y-m-d H:i");
+    $update= $date;
+    $isDisabled= 0;
+    $sql = "INSERT INTO user (username, password, roles, image, created_at, updated_at, is_disabled) VALUES ('$pseudo', '$password', '$role', '$img', '$date', '$update', '$isDisabled')";
     $conn = connection();
     $stmt = $conn->prepare($sql);
     $stmt->execute([$pseudo, $password]);
