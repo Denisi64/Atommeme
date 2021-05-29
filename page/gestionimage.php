f6ff3c71d0c5095bcbea588dffb6b332cb9a8500
Denisi64 <simon.denis64@gmail.com>
2021-05-29 17:07:38 +0200

    img

---
+++ page/gestionimage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+      integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="../css/datatables.min.css"/>
+
+<div class="container-fluid">
+    <?php
+    include '../partials/navbar.php'
+    ?>
+</div>
+
+<table class="table table-hover" id="myTable">
+    <thead>
+    <tr>
+        <th>Column 1</th>
+        <th>Column 2</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Row 1 Data 1</td>
+        <td>Row 1 Data 2</td>
+    </tr>
+    <tr>
+        <td>Row 2 Data 1</td>
+        <td>Row 2 Data 2</td>
+    </tr>
+    </tbody>
+</table>
+
+<script src="/static/js/datatables.min.js"></script>
+
+</html>
+
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
-if(isset($_SESSION["pseudo"])){
+if (isset($_SESSION["pseudo"])) {
     $pseudoHtml = $_SESSION["pseudo"];
 }
 ?>
+<link rel="stylesheet" href="../css/style.css">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="../page/index.php">Atommeme</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
@@ -19,14 +20,19 @@ if(isset($_SESSION["pseudo"])){
             </li>
             <?php
             if (isset($_SESSION['roles']) && $_SESSION['roles'] == 2) {
-                echo '<li class="nav-item dropdown">
+                echo '<li class="nav-item active">
+                        <a class="nav-link" href="../page/gestionimage.php">Gestion Image
+                            <span class="sr-only">(current)</span>
+                        </a>
+                      </li>
+            <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
                             aria-expanded="false">Bonjour, 
                             <?= $pseudoHtml ?> 
                             </a>
                 <div class="dropdown-menu">
                     <a class="dropdown-item" href="../page/myaccount.php">Mon Compte</a>
-                    <a class="dropdown-item" href="../partials/sessiondown.php">Déconexion</a>
+                    <a class="dropdown-item deco" href="../partials/sessiondown.php">Déconexion</a>
                 </div>
             </li>';
 
@@ -36,7 +42,7 @@ if(isset($_SESSION["pseudo"])){
                    aria-expanded="false">Connexion</a>
                 <div class="dropdown-menu">
                     <a class="dropdown-item" href="../page/register.php">Inscrivez vous</a>
-                    <a class="dropdown-item" href="../page/login.php">Connectez vous</a>
+                    <a class="dropdown-item"  href="../page/login.php">Connectez vous</a>
                 </div>
             </li>';
             }
@@ -45,114 +51,4 @@ if(isset($_SESSION["pseudo"])){
         </ul>
     </div>
 </nav>
-<?php
-//if (session_status() == PHP_SESSION_NONE) {
-//    echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-//    <a class="navbar-brand" href="../index.php">Atommeme</a>
-//    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
-//            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-//        <span class="navbar-toggler-icon"></span>
-//    </button>
-//    <div class="collapse navbar-collapse" id="navbarColor02">
-//        <ul class="navbar-nav mr-auto">
-//            <li class="nav-item active">
-//                <a class="nav-link" href="../page/allimage.php">Image
-//                    <span class="sr-only">(current)</span>
-//                </a>
-//            </li>
-//            <li class="nav-item dropdown">
-//                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-//                   aria-expanded="false">Connexion</a>
-//                <div class="dropdown-menu">
-//                    <a class="dropdown-item" href="../page/register.php">Inscrivez vous</a>
-//                    <a class="dropdown-item" href="../page/login.php">Connectez vous</a>
-//                </div>
-//            </li>
-//        </ul>
-//    </div>
-//</nav>';
-//} else {
-//    if ($_SESSION['roles'] == 2) {
-//        echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-//    <a class="navbar-brand" href="../index.php">Atommeme</a>
-//    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
-//            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-//        <span class="navbar-toggler-icon"></span>
-//    </button>
-//    <div class="collapse navbar-collapse" id="navbarColor02">
-//        <ul class="navbar-nav mr-auto">
-//            <li class="nav-item active">
-//                <a class="nav-link" href="../page/allimage.php">Image
-//                    <span class="sr-only">(current)</span>
-//                </a>
-//            </li>
-//            <li class="nav-item dropdown">
-//                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-//                   aria-expanded="false">Bonjour</a>';
-//        echo $_SESSION["pseudo"];
-//        echo '<div class="dropdown-menu">
-//                    <a class="dropdown-item" href="../page/register.php">Mon compte</a>
-//                    <a class="dropdown-item" href="../page/login.php">Se deconecter</a>
-//                </div>
-//            </li>
-//        </ul>
-//    </div>
-//</nav>';
-//
-//    } elseif ($_SESSION['roles'] == 1) {
-//        echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-//    <a class="navbar-brand" href="../index.php">Atommeme</a>
-//    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
-//            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-//        <span class="navbar-toggler-icon"></span>
-//    </button>
-//    <div class="collapse navbar-collapse" id="navbarColor02">
-//        <ul class="navbar-nav mr-auto">
-//            <li class="nav-item active">
-//                <a class="nav-link" href="../page/allimage.php">Image
-//                    <span class="sr-only">(current)</span>
-//                </a>
-//            </li>
-//            <li class="nav-item dropdown">
-//                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-//                   aria-expanded="false">Bonjour</a>';
-//        echo $_SESSION["pseudo"];
-//        echo '<div class="dropdown-menu">
-//                    <a class="dropdown-item" href="../page/register.php">Mon compte</a>
-//                    <a class="dropdown-item" href="../page/login.php">Se deconecter</a>
-//                </div>
-//            </li>
-//        </ul>
-//    </div>
-//</nav>';
-//    } elseif ($_SESSION['roles'] == 0) {
-//        echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-//    <a class="navbar-brand" href="../index.php">Atommeme</a>
-//    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
-//            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-//        <span class="navbar-toggler-icon"></span>
-//    </button>
-//    <div class="collapse navbar-collapse" id="navbarColor02">
-//        <ul class="navbar-nav mr-auto">
-//            <li class="nav-item active">
-//                <a class="nav-link" href="../page/allimage.php">Image
-//                    <span class="sr-only">(current)</span>
-//                </a>
-//            </li>
-//            <li class="nav-item dropdown">
-//                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-//                   aria-expanded="false">Bonjour</a>';
-//        echo $_SESSION["pseudo"];
-//        echo '<div class="dropdown-menu">
-//                    <a class="dropdown-item" href="../page/register.php">Mon compte</a>
-//                    <a class="dropdown-item" href="../page/login.php">Se deconecter</a>
-//                </div>
-//            </li>
-//        </ul>
-//    </div>
-//</nav>';
-//    }
-//}
-
-?>
 
